b8647660db99fdff2bef4dad08e17f7e38d6132a
ryanmaraujo <121063462+ryanmaraujo@users.noreply.github.com>
2023-08-23 19:58:32 -0300

    Add files via upload

--- PRO_1-4_C83_TemplateDoProjeto-main/screens/RideHistory.js
+++ PRO_1-4_C83_TemplateDoProjeto-main/screens/RideHistory.js
@@ -156,6 +156,13 @@ export default class RideHistoryScreen extends Component {
           </View>
         </View>
         <View style={styles.lowerContainer}>
+          <FlatList
+            data = {allTransactions}
+            renderItem={this.renderItem}
+            keyExtractor={(item,index) => index.toString ()}
+            onEndReached={() => this.fetchMoreTransactions (searchText)}
+            onEndReachedThreshold={0.7}
+          />
         </View>
       </View>
     );
